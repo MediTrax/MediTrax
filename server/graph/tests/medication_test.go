29301ee0ac96939c8d39c1e561392b999121301b
Ccindy0171 <cindy.w0135@gmail.com>
2024-11-21 20:54:16 +0800

    misc: merged, created test helper

--- server/graph/tests/medication_test.go
+++ server/graph/tests/medication_test.go
@@ -12,60 +12,7 @@ func TestMedication(t *testing.T) {
 	// c := client.New(handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}})))
 	c := client.New(NewServer())
 
-	var username = "test_med3"
-	var password = "test_med"
-	var phoneNumber = "9947329479"
-	var accessToken string
-	var userId string
-
-	t.Run("Create User", func(t *testing.T) {
-		var resp struct {
-			CreateUser struct {
-				UserID  string
-				Message string
-			}
-		}
-
-		err := c.Post(`mutation($phoneNumber: String!, $password: String!, $username: String!) {
-			createUser(
-				phoneNumber: $phoneNumber,
-				password: $password,
-				username: $username,
-				role: "patient"
-			){
-				userId,
-				message
-			}
-		}`, &resp, client.Var("phoneNumber", phoneNumber), client.Var("password", password), client.Var("username", username))
-
-		if err != nil {
-			println("Errors:")
-			println(err.Error())
-			println("End of errors")
-		}
-		require.Equal(t, fmt.Sprintf("User %s created successfully", username), resp.CreateUser.Message)
-	})
-
-	t.Run("Login", func(t *testing.T) {
-		var resp struct {
-			LoginUser struct {
-				UserID string
-				Token  struct{ AccessToken string }
-			}
-		}
-
-		c.MustPost(fmt.Sprintf(`mutation login {
-			loginUser(phoneNumber: "%s", password:"%s"){
-				userId
-			  token{
-				accessToken
-			  }
-			}
-		  }`, phoneNumber, password), &resp)
-
-		accessToken = resp.LoginUser.Token.AccessToken
-		userId = resp.LoginUser.UserID
-	})
+	user := CreateUserAndLogin(t, c)
 
 	t.Run("Create Medication", func(t *testing.T) {
 		var response struct {
@@ -86,24 +33,10 @@ func TestMedication(t *testing.T) {
 				medicationId
 				message
 		  	}
-		  }`, &response, client.AddHeader("Authorization", fmt.Sprintf("Bearer %s", accessToken)))
+		  }`, &response, client.AddHeader("Authorization", fmt.Sprintf("Bearer %s", user.AccessToken)))
 
 		require.Equal(t, "Medication test_medication added successfully", response.AddMedication.Message)
 	})
 
-	t.Run("Delete User", func(t *testing.T) {
-		var response struct {
-			DeleteUser struct {
-				Message string
-			}
-		}
-
-		c.MustPost(`mutation delete_user{
-			deleteUser{
-				message
-		  	}
-		  }`, &response, client.AddHeader("Authorization", fmt.Sprintf("Bearer %s", accessToken)))
-
-		require.Equal(t, fmt.Sprintf("User %s with name %s deleted successfully", userId, username), response.DeleteUser.Message)
-	})
+	DeleteUser(t, c, user)
 }
