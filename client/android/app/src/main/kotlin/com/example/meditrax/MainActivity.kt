f24654540d80dc254f20b0f8e6ea35e9a0b089e9
Wangxuran <wangxura22@mails.tsinghua.edu.cn>
2024-12-15 23:43:54 +0800

    try to commit

--- client/android/app/src/main/kotlin/com/example/meditrax/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.meditrax
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- client/lib/providers/graphql_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:meditrax/providers/graphql.dart';
-
-class GraphQLTest extends GraphQLService {
-  @override
-  GraphQLClient build() {
-    throw UnimplementedError();
-  }
-}
